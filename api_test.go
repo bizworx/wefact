902724505cafaeaf28401a5844a884016764e921
Jeroen Grusewski <jeroen@bizworx.nl>
2020-11-25 11:56:17 +0100

    cleanup client

--- api_test.go
+++ api_test.go
@@ -1,13 +1,14 @@
 package wefact
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestApi(t *testing.T) {
-	client := New()
+	client := New(os.Getenv("WEFACT_API_KEY"))
 	//var results map[string]interface{}
 
 	_, err := client.Request("invoice", "list", nil)
